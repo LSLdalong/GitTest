baeb329f7a787d89e9a57751a0dfdb0bb56e569a
LSL <405585058@qq.com>
2015-11-24 23:01:55 +0800

    第二次

---
+++ GitTest/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  GitTest
+//
+//  Created by 大龙 on 15/11/24.
+//  Copyright © 2015年 大龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+-(void)test1{
+    
+}
+-(void)test2{
+    
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
